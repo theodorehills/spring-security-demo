f7bb3f4af1c264ac94c0e7f75502fa5f6d9202c2
Zihlu Wang <zihlu.wang@outlook.com>
2023-09-09 02:23:10 +0800

    feat: Replaced HttpMethod with RequestMethod in Spring Framework

--- src/main/java/wang/zihlu/springsecuritydemo/config/properties/CorsProperties.java
+++ src/main/java/wang/zihlu/springsecuritydemo/config/properties/CorsProperties.java
@@ -2,7 +2,7 @@ package wang.zihlu.springsecuritydemo.config.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import wang.zihlu.springsecuritydemo.constant.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * CorsProperties
@@ -14,12 +14,24 @@ import wang.zihlu.springsecuritydemo.constant.HttpMethod;
 @ConfigurationProperties(prefix = "app.cors")
 public class CorsProperties {
 
+    /**
+     * Origins that can access this system.
+     */
     private String[] allowedOrigins = {"*"};
 
-    private HttpMethod[] allowedMethods = {HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+    /**
+     * Allowed request methods.
+     */
+    private RequestMethod[] allowedMethods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE};
 
+    /**
+     * Allowed headers.
+     */
     private String[] allowedHeaders = {"Content-Type", "Authorization"};
 
+    /**
+     * Headers that will be exposed to the front-end.
+     */
     private String[] exposedHeaders = {"Content-Type", "Authorization"};
 
     private Boolean allowCredentials;
